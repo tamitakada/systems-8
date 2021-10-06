bf59104fcf120110d9a7030a857f8eeabd167550
tamitakada <tamitakada@gmail.com>
2021-10-06 16:40:22 -0400

    added cat and cmp

--- str_funcs.c
+++ str_funcs.c
@@ -10,18 +10,41 @@ int mystrlen( char *s ) {
 }
 
 char * mystrcpy( char *dest, char *source ) {
-  int i = 0;
-  while (*source) {
-    dest[i] = source[i];
-    i++;
-    source++;
-  }
-  dest[i] = '\0';
-  return dest;
+    int i = 0;
+    while (source[i]) {
+        dest[i] = source[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
 }
 
-/*
-char * mystrcat( char *dest, char *source );
-int mystrcmp( char *s1, char *s2 );
-char * mystrchr( char *s, char c );
-*/
+
+char * mystrcat( char *dest, char *source ) {
+    int end = mystrlen(dest);
+    int i = 0;
+    while (source[i]) {
+        dest[end + i] = source[i];
+        i++;
+    }
+    return dest;
+}
+
+int mystrcmp( char *s1, char *s2 ) {
+    while (*s1) {
+        if ((*s1) > (*s2)) return 1;
+        else if ((*s1) < (*s2)) return -1;
+        else {
+            s1++;
+            s2++;
+        }
+    }
+    
+    if ((*s1) == (*s2)) return 0;
+    return -1;
+}
+
+//char * mystrchr( char *s, char c ) {
+//
+//}
+
